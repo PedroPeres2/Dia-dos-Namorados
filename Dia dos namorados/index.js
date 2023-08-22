867f402e9907708d82e741e000884019e8e613cf
Pedro Peres <96542639+PedroPeres2@users.noreply.github.com>
2023-08-22 13:39:53 -0300

    Add files via upload

---
+++ Dia dos namorados/index.js
@@ -0,0 +1,16 @@
+function sim(){
+    alert("Você aceitou viver pra sempre comigo!!!!! :) ");
+}
+function desvia(t){
+    var btn = t;
+    btn.style.position = "absolute";
+    btn.style.bottom = geraPosicao(10,90);
+    btn.style.left = geraPosicao(10,90);
+    console.log("Ih mane ta desviando hihi");
+}
+
+
+function geraPosicao(min, max){
+    return (Math.random() * (max-min) + min) + '%';
+};
+
